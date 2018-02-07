adcf7df8b0d676e1ce938a5349c140d7479a4d39
fleita9 <fleita@gmail.com>
2018-02-07 16:33:11 -0300

    agrego observe before save para asegurarme que las propiedades que tiene que ser ObjectId lo sean

--- objectid-type.js
+++ objectid-type.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var debug = require('debug')('loopback:mixins:objectid-type');
+const debug = require('debug')('loopback:mixins:objectid-type');
+const ObjectId = require('mongodb').ObjectId;
 
 module.exports = (Model, options) => {
-    var objectId = Model.getDataSource().connector.getDefaultIdType();
-    var isMongoConnector = Model.getDataSource().connector.name === 'mongodb';
-    var isPropertiesExists = options && options.properties && Array.isArray(options.properties);
+    const objectId = Model.getDataSource().connector.getDefaultIdType();
+    const isMongoConnector = Model.getDataSource().connector.name === 'mongodb';
+    const isPropertiesExists = options && options.properties && Array.isArray(options.properties);
 
     if (!isPropertiesExists) {
         debug('Properties not found for ' + Model.definition.name);
@@ -23,4 +24,33 @@ module.exports = (Model, options) => {
         debug('loopback-connector-mongodb not found for current model');
     }
 
+
+    Model.observe('before save', function(ctx, next) {
+        if (!isPropertiesExists) {
+            debug('Properties not found for ' + Model.definition.name);
+            throw 'Properties not found for ' + Model.definition.name;
+        }
+
+        if (isMongoConnector) {
+            if(ctx.instance) {
+                options.properties.map(propertyKey => {
+                    if(!(ctx.instance[propertyKey] instanceof objectId)) {
+                        ctx.instance[propertyKey] = new ObjectId(ctx.instance[propertyKey]);
+                    }
+                })
+                next();
+            }
+            if(ctx.data) {
+                options.properties.map(propertyKey => {
+                    if(!(ctx.data[propertyKey] instanceof objectId)) {
+                        ctx.data[propertyKey] = new ObjectId(ctx.data[propertyKey]);
+                    }
+                })
+                next();
+            }
+        } else {
+            throw 'loopback-connector-mongodb not found for current model'
+            debug('loopback-connector-mongodb not found for current model');
+        }
+    })
 };
